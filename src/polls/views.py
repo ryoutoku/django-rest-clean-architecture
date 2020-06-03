083890b06a530994a8bdf4fd584a282734800d5f
Akinori Hayashi <invisiblechain@hotmail.co.jp>
2020-06-03 22:51:42 +0900

    feat: 説明用にpost実装

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,11 +1,24 @@
-from rest_framework import viewsets
-from .serializers import QuestionSerializer, ChoiceSerializer
-from .models import Question, Choice
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Choice, Question
+from .serializers import ChoiceSerializer, QuestionSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def create(self, request, *args, **kwargs):
+        # POSTで /api/polls/questions/ を実行した場合に実行されるmethod
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED,
+                        headers=headers)
 
 
 class ChoiceViewSet(viewsets.ModelViewSet):
